1a253b7b15f03bbfe0159f85832d763fc2888263
Gabriel <gabrielnogueira4014@gmail.com>
2025-03-28 20:02:11 -0300

    refactor yoeo trt class

--- yoeo/scripts/runTrt.py
+++ yoeo/scripts/runTrt.py
@@ -5,35 +5,23 @@ import pycuda.driver as cuda
 import pycuda.autoinit
 
 class YOEOInference:
-    def __init__(self, trt_model_path, target_size=(416, 416)):
-        self.target_size = target_size
-        self.engine = None
-        self.context = None
-        self.input_memory = None
-        self.detections_memory = None
-        self.segmentations_memory = None
-        self.stream = None
-
-        ## Init routine
+    def __init__(self, trt_model_path):
         # Load model
         self.load_trt_model(trt_model_path)
-        # allocar memoria de acordo com o target_size
-        self.get_io_shapes() #Values of image_input, detections_shape, segmentations_shape are updated to the class
+
+        # Declare input/outputs shapes based on the model
+        self.get_io_shapes()
+
+        # Allocate memory
         self.allocate_memory()
 
-
-    def preprocess_image(self, image):
-        image_resized = cv2.resize(image, self.target_size)
-        image_transposed = np.transpose(image_resized, (2, 0, 1))  # HWC to CHW
-        image_normalized = image_transposed / 255.0  # Normalize to [0, 1]
-        image_input = np.expand_dims(image_normalized.astype(np.float32), axis=0)  # Add batch dimension
-        image_input = np.ascontiguousarray(image_input)  # Ensure the array is contiguous
-        return image_input
+        self.stream = cuda.Stream()
 
     def load_trt_model(self, trt_model_path):
         TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
         with open(trt_model_path, "rb") as f, trt.Runtime(TRT_LOGGER) as runtime:
             self.engine = runtime.deserialize_cuda_engine(f.read())
+
         # Run after loading the model
         self.context = self.engine.create_execution_context()
 
@@ -42,83 +30,78 @@ class YOEOInference:
         self.input_shape = self.engine.get_tensor_shape("InputLayer")
         self.detections_shape = self.engine.get_tensor_shape("Detections")
         self.segmentations_shape = self.engine.get_tensor_shape("Segmentations")
-
-        #return self.input_shape, self.detections_shape, self.segmentations_shape
+        self.target_shape = self.input_shape[-2:] # get the two last dimensions of input shape: (416,416)
 
     def allocate_memory(self):
-        # Allocate device memory for input and outputs
+        # Allocate cuda memory for input and outputs
         self.input_memory = cuda.mem_alloc(int(np.prod(self.input_shape)) * np.dtype(np.float32).itemsize)
         self.detections_memory = cuda.mem_alloc(int(np.prod(self.detections_shape)) * np.dtype(np.float32).itemsize)
         self.segmentations_memory = cuda.mem_alloc(int(np.prod(self.segmentations_shape)) * np.dtype(np.float32).itemsize)
-        self.stream = cuda.Stream()
 
-    def run_inference(self, image_input):           
+        # Allocate cpu memory for inputs and outputs
+        self.detections_output = np.empty(self.detections_shape, dtype=np.float32)
+        self.segmentations_output = np.empty(self.segmentations_shape, dtype=np.float32)
+        self.segmented_image = np.zeros(self.target_shape, dtype=np.uint8)
+
+    def preprocess_image(self, image):
+        input_image = cv2.resize(image, self.target_shape)
+        input_image = np.transpose(input_image, (2, 0, 1))  # HWC to CHW
+        input_image = input_image / 255.0  # Normalize to [0, 1]
+        input_image = np.expand_dims(input_image.astype(np.float32), axis=0)  # Add batch dimension
+        input_image = np.ascontiguousarray(input_image)  # Ensure the array is contiguous
+        
+        return input_image
+
+    def run_inference(self, input_image):           
         # Transfer input data to the GPU
-        cuda.memcpy_htod_async(self.input_memory, image_input, self.stream)
+        cuda.memcpy_htod_async(self.input_memory, input_image, self.stream)
 
         # Run inference
         bindings = [int(self.input_memory), int(self.detections_memory), int(self.segmentations_memory)]
         self.context.execute_async_v2(bindings=bindings, stream_handle=self.stream.handle)
 
         # Transfer output data back to the host
-        detections_output = np.empty(self.detections_shape, dtype=np.float32)
-        segmentations_output = np.empty(self.segmentations_shape, dtype=np.float32)
-        cuda.memcpy_dtoh_async(detections_output, self.detections_memory, self.stream)
-        cuda.memcpy_dtoh_async(segmentations_output, self.segmentations_memory, self.stream)
+        cuda.memcpy_dtoh_async(self.detections_output, self.detections_memory, self.stream)
+        cuda.memcpy_dtoh_async(self.segmentations_output, self.segmentations_memory, self.stream)
 
         # Synchronize the stream
         self.stream.synchronize()
-        return detections_output, segmentations_output
+        return self.detections_output, self.segmentations_output
 
     def postprocess_segmentations(self, segmentations_output):
-        segmentations_array = np.array(segmentations_output).squeeze()  # Remove batch dimension if needed
+        segmentations_array = np.array(segmentations_output).squeeze()  # Remove batch dimension
+        self.segmented_image[segmentations_array == 1] = 255  # Field markings
+        self.segmented_image[segmentations_array == 2] = 127  # Field carpet
 
-        # Create a blank image for visualization
-        segmented_image = np.zeros_like(segmentations_array, dtype=np.uint8)
-        segmented_image[segmentations_array == 1] = 255  # Field markings
-        segmented_image[segmentations_array == 2] = 127  # Field carpet
-
-        #return resultado
-
-        return segmented_image
+        return self.segmented_image
 
     def __call__(self, img):
-        """ 
-        Run YOEO Inference with TensorRT model 
-        Args:
-            img (np.ndarray): Input image to run inference on
-        Returns:
-            np.ndarray: Segmented image
-        """
         # Load and preprocess image
-        image_input = self.preprocess_image(img)
-
-        
-    
+        input_image = self.preprocess_image(img)
 
         # Run inference
-        detections_output, segmentations_output = self.run_inference(image_input)
+        _, self.segmentations_output = self.run_inference(input_image)
 
         # Postprocess and visualize results
-        segmented_image = self.postprocess_segmentations(segmentations_output)
+        self.segmented_image = self.postprocess_segmentations(self.segmentations_output)
         
-        cv2.imwrite("segmented_result_trt.jpg", segmented_image)
-        return segmented_image
+        return self.segmented_image
 
 
 if __name__ == "__main__":
-    import argparse
-    
-    # Setup argparse to handle command line arguments
-    parser = argparse.ArgumentParser(description="Run YOEO Inference with TensorRT model")
-    parser.add_argument("image_path", type=str, help="Path to the input image")
-    parser.add_argument("trt_model_path", type=str, help="Path to the TensorRT model")
+    import time
 
-    # Parse the arguments
-    args = parser.parse_args()
+    trt_model_path = f'/home/drones/Documents/rc-fork/YOEO/config/model_fp16.trt'
+    image_path = f'/home/drones/Documents/rc-fork/YOEO/data/samples/frame0256.jpg'
 
     # Run YOEO Inference
-    yoeo = YOEOInference(args.trt_model_path)
-    img = cv2.imread(args.image_path)
-    yoeo(img)
+    yoeo = YOEOInference(trt_model_path)
+    
+    img = cv2.imread(image_path)
 
+    for i in range(10):
+        t0 = time.time()
+        segmented_img = yoeo(img)
+        print(f'elapsed time: {time.time() - t0:.3f}')
+
+    cv2.imwrite('test.jpg', segmented_img)
